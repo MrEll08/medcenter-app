65d75ed72ffd75bf4766b6fd759da3434d9ff5dc
Max Gavrilov <maxim.gavrilov.2008@gmail.com>
2025-07-28 23:19:06 +0300

    created db connection and alembic migrator

---
+++ app/db/models/base.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID, TIMESTAMP
+from sqlalchemy.sql import func
+import uuid
+
+from app.db import DeclarativeBase
+
+
+class BaseTable(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=func.gen_random_uuid(),
+        unique=True,
+        doc="Unique index of element (type UUID)",
+    )
+    dt_created: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.current_timestamp(),
+        nullable=False,
+        doc="Date and time of create (type TIMESTAMP)",
+    )
+    dt_updated: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.current_timestamp(),
+        server_onupdate=func.current_timestamp(),
+        nullable=False,
+        doc="Date and time of update (type TIMESTAMP)",
+    )
+
+    def __repr__(self) -> str:
+        cols = ", ".join(f"{c.name}={getattr(self, c.name)!r}" for c in self.__table__.columns)
+        return f"<{self.__tablename__}: {cols}>"
