8d01548f8fa092b7e28ea8e4fded8cbc3127cfe6
Max Gavrilov <maxim.gavrilov.2008@gmail.com>
2025-11-19 03:08:43 +0500

    feat(security): add allowed host

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({mode}) => {
                     changeOrigin: true,
                 },
             },
+            allowedHosts: ["alapaevsk-pc2"],
         },
     }
 })
